0707ecaaccecc97244b94d75fc53ccc28722b7f4
Atomic <semkina.an@yandex.ru>
2025-03-24 23:51:39 +0300

    Эксперименты с функциями

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println(2+2);
+        System.out.println(3-1);
+        System.out.println(3*8);
+        System.out.println(6/2);
+        System.out.println("Привет"+", мир");
     }
 }
