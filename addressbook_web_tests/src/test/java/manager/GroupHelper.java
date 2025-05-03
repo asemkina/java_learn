85106961f43e63261d16fcd3e0f2a052384bd1ae
Atomic <semkina.an@yandex.ru>
2025-05-04 01:25:58 +0300

    Цикл: перебор элементов коллекции removeAllGroups

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -77,4 +77,18 @@ public class GroupHelper extends HelperBase{
         openGroupsPage();
         return manager.driver.findElements(By.name("selected[]")).size();
     }
+
+    public void removeAllGroups() {
+        openGroupsPage();
+        selectAllGroups();
+        removeSelectedGroup();
+        returnToGroupsPage();
+    }
+
+    private void selectAllGroups() {
+        var checkboxes = manager.driver.findElements(By.name("selected[]"));
+        for (var checkbox : checkboxes){
+            checkbox.click();
+        }
+    }
 }
