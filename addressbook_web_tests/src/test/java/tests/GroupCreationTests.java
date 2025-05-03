7c3809887eb29fad4894644d54062e6a319f2811
Atomic <semkina.an@yandex.ru>
2025-05-04 02:16:38 +0300

    Цикл со счётчиком CanCreateMultipleGroups

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -33,5 +33,16 @@ public class GroupCreationTests extends TestBase {
     public void CanCreateGroupWithFooterOnly() {
         app.groups().CreateGroup(new GroupData().withFooter("some footer"));
     }
+
+    @Test
+    public void CanCreateMultipleGroups() {
+        int n = 5;
+        int groupCount = app.groups().getCount();
+        for (int i=0; i<n; i++) { ///переменная i является счетчиком, на каждой итерации к i прибавляется 1(инкремент)
+            app.groups().CreateGroup(new GroupData(randomString(i * 10), "Group name", "Group footer"));
+        }
+        int newGroupCount = app.groups().getCount();
+        Assertions.assertEquals(groupCount + n, newGroupCount);
+    }
 }
 
