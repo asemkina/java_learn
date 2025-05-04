05f063d2f6e5c2eb389d3ac71ad9c9cb37f03f8b
Atomic <semkina.an@yandex.ru>
2025-05-04 23:39:13 +0300

    Сравнение списков

--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -15,12 +15,15 @@ public class GroupCreationTests extends TestBase {
         for (var title : List.of("","group title")) {
             for (var name : List.of("","group name")){
                 for (var footer : List.of("", "group footer")){
-                    result.add(new GroupData(title, name, footer));
+                    result.add(new GroupData().withTitle(title).withName(name).withFooter(footer));
                 }
             }
         }
         for (int i=0; i<5; i++) { ///переменная i является счетчиком, на каждой итерации к i прибавляется 1(инкремент)
-            result.add(new GroupData(randomString(i * 5), randomString(i * 5), randomString(i * 5)));
+            result.add(new GroupData()
+                    .withTitle(randomString(i * 5))
+                    .withName(randomString(i * 5))
+                    .withFooter(randomString(i * 5)));
         }
         return result;
     }
@@ -36,7 +39,7 @@ public class GroupCreationTests extends TestBase {
 
     public static List<GroupData> negativeGroupProvider() {
         var result = new ArrayList<GroupData>(List.of(
-                new GroupData ("group title '", "", "")));
+                new GroupData ("", "group title '", "", "")));
         return result;
     }
 
--- addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupModificationTests.java
@@ -8,7 +8,7 @@ public class GroupModificationTests extends TestBase {
     @Test
     void canModifyGroup() {
         if (app.groups().getCount() == 0 ) {
-            app.groups().CreateGroup(new GroupData("Group1", "Group name", "Group footer"));
+            app.groups().CreateGroup(new GroupData("", "Group1", "Group name", "Group footer"));
         }
         app.groups().ModifyGroup(new GroupData().withTitle("modifiedTitle"));
     }
