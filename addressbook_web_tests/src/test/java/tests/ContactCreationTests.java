75279192a50aefd9d409abfe13969d62e0d26241
Atomic <semkina.an@yandex.ru>
2025-05-04 22:33:41 +0300

    ParameterizedTest ContactProvider+negativeContactProvider

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,17 +1,12 @@
 package tests;
 
 import model.ContactData;
-import model.GroupData;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class ContactCreationTests extends TestBase {
 
@@ -28,19 +23,16 @@ public class ContactCreationTests extends TestBase {
                 }
             }
         }
-        for (int i=0; i<5; i++) { ///переменная i является счетчиком, на каждой итерации к i прибавляется 1(инкремент)
+        for (int i = 0; i < 5; i++) {
             result.add(new ContactData(randomString(i * 4), randomString(i * 4), randomString(i * 4), randomString(i * 4), randomString(i * 4)));
         }
         return result;
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"Anna", "Anna '"})
-    public void canCreateСontact(String firstName) {
-        int contactCount = app.contacts().getCount();
-        app.contacts().createContact(new ContactData(firstName, "Семкина", "ул.Богдана Хмельницкого, 3-62", "89191990515", "semkina.an@yandex.ru"));
-        int newContactCount = app.contacts().getCount();
-        Assertions.assertEquals(contactCount + 1, newContactCount);
+    public static List<ContactData> negativeContactProvider() {
+        var result = new ArrayList<ContactData>(List.of(
+                new ContactData("Anna '", "", "", "", "")));
+        return result;
     }
 
     @ParameterizedTest
@@ -52,29 +44,13 @@ public class ContactCreationTests extends TestBase {
         Assertions.assertEquals(contactCount + 1, newContactCount);
     }
 
-    @Test
-    public void canCreateСontactWithFirstNameOnly() {
-        app.contacts().createContact(new ContactData().withName("Anna"));
-    }
-
-    @Test
-    public void canCreateСontactWithLastNameOnly() {
-        app.contacts().createContact(new ContactData().withLastName("Semkina"));
-    }
-
-    @Test
-    public void canCreateСontactWithAddressOnly() {
-        app.contacts().createContact(new ContactData().withAddress("Bryansk"));
-    }
-
-    @Test
-    public void canCreateСontactWithPhoneOnly() {
-        app.contacts().createContact(new ContactData().withPhone("9999999999"));
-    }
-
-    @Test
-    public void canCreateСontactWithEmailOnly() {
-        app.contacts().createContact(new ContactData().withEmail("Anna@yandex.ru"));
+    @ParameterizedTest
+    @MethodSource("negativeContactProvider")
+    public void CanNotCreateContact(ContactData contact) {
+        int contactCount = app.contacts().getCount();
+        app.contacts().createContact(contact);
+        int newContactCount = app.contacts().getCount();
+        Assertions.assertEquals(contactCount, newContactCount);
     }
 
 }
