a464b50f02492c4a9262ef9add77c023886ed8d1
Atomic <semkina.an@yandex.ru>
2025-05-15 01:38:40 +0300

    Словари

--- addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactInfoTests.java
@@ -7,18 +7,19 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ContactInfoTests extends TestBase{
+public class ContactInfoTests extends TestBase {
     @Test
-    void testPhones(){
+    void testPhones() {
         if (app.hbm().getContactCount() == 0) {
             app.hbm().сreateContact(new ContactData("", "Name", "Lastname", "Address", "99999999", "Email", "", "88919199", "", ""));
         }
-    var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
-        var phones = app.contacts().getPhones(contact);
-        var expected = Stream.of(contact.home(), contact.mobile(), contact.work(), contact.phone2()) /// забираем все телефоны из контакта
-                .filter(s -> s !=null && !s.isEmpty()) /// пропускаем пустые номера: строка не null и не пустая строка
-                .collect(Collectors.joining("\n")); ///склеиваем строки, разделитель - перенос строки
+        var contacts = app.hbm().getContactList();
+        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+                Stream.of(contact.home(), contact.mobile(), contact.work(), contact.phone2()) /// забираем все телефоны из контакта
+                        .filter(s -> s != null && !s.isEmpty()) /// пропускаем пустые номера: строка не null и не пустая строка
+                        .collect(Collectors.joining("\n")) ///склеиваем строки, разделитель - перенос строки
+        ));
+        var phones = app.contacts().getPhones();
         Assertions.assertEquals(expected, phones);
     }
 }
