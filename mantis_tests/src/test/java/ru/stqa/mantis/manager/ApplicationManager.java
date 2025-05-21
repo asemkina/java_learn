a2825346191e9e29967aa1c51bd7d11b3fb40ece
Atomic <semkina.an@yandex.ru>
2025-05-21 10:24:08 +0300

    mantis_tests

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,43 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+    }
+
+
+    public WebDriver driver() {
+        if (driver == null) {
+            if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            } else if ("firefox".equals(browser)) {
+                driver = new FirefoxDriver();
+            } else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseUrl"));
+        }
+        return driver;
+    }
+
+    public SessionHelper session(){
+        if (sessionHelper == null){
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+
+}
